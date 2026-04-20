abcb8c68f2fbe4fd5ec0b1fc3a1d24b95d3dcd2d
damiluj04 <167890462+damiluj04@users.noreply.github.com>
2026-04-20 15:49:38 -0500

    Refactor reviewInput function for clarity

--- hw3.js
+++ hw3.js
@@ -355,40 +355,42 @@ function validateConPassword()
 function reviewInput()
 {
     var formcontent = document.getElementById("signup");
-    var formoutput;
-    var i;
-    formoutput = "<table class='output'><th colspan = '3'>Your Information:</th>";
-    for (i = 0; i < formcontent.length; i++)
+    var formoutput = "<table class='output'><th colspan = '3'>Your Information:</th>";
+    
+    for (i = 0; i < formcontent.length; i++)  
     {    
-        if (formcontent.elements[i].value !== "")
-        {
-            datatype = formcontent.elements[i].type;
-            switch (datatype)
-            {
-                case "checkbox":
-                    if (formcontent.elements[i].checked)
-                    {
-                        formoutput += "<tr> <td align= 'right'>" + formcontent.elements[i].name + "</td>";
-                        formoutput += "<td class= 'outputdata'>&#x2713; </td></tr>";
-                    }
-                    break;
-                
-                case "radio":
-                    if (formcontent.elements[i].checked)
-                    {
-                        formoutput += "<tr> <td align = 'right'>" + formcontent.elements[i].name + "</td>";
-                        formoutput += "<td class = 'outputdata'>" + formcontent.elements[i].value + "</td></tr>";
-                    }
-                    break;
+        var el = formcontent.elements[i];
+        var datatype = el.type;
+        var name = el.name;
+        var value = el.value;
 
-                case "button":
-                case "submit":
-                case "reset":
-                    break;
-                default:
+        if (!name) continue;
+
+        switch (datatype)
+        {
+            case "checkbox":
+                if (formcontent.elements[i].checked)
+                {
                     formoutput += "<tr> <td align= 'right'>" + formcontent.elements[i].name + "</td>";
-                    formoutput += "<td class= 'outputdata'>" + formcontent.elements[i].value + "</td></tr>";
-            }
+                    formoutput += "<td class= 'outputdata'>&#x2713; </td></tr>";
+                }
+                break;
+                
+            case "radio":
+                if (formcontent.elements[i].checked)
+                {
+                    formoutput += "<tr> <td align = 'right'>" + formcontent.elements[i].name + "</td>";
+                    formoutput += "<td class = 'outputdata'>" + formcontent.elements[i].value + "</td></tr>";
+                }
+                break;
+
+            case "button":
+            case "submit":
+            case "reset":
+                break;
+            default:
+                formoutput += "<tr> <td align= 'right'>" + formcontent.elements[i].name + "</td>";
+                formoutput += "<td class= 'outputdata'>" + formcontent.elements[i].value + "</td></tr>";
         }
     }
 
